6249b5eaacb0724d98f1efa907bcb17959ad91e9
Riad BENRADI <riad.be@kuikops.com>
2025-08-25 10:44:31 +0200

    Jenkins file done

---
+++ backend/Jenkinsfile.groovy
@@ -0,0 +1,36 @@
+pipeline {
+    agent any
+    triggers {
+        githubPush()
+    }
+    stages {
+        stage('Checkout') {
+            steps {
+                git(branch: 'main', url: 'https://github.com/RiadBRD/Library.git')
+            }
+        }
+
+        stage('Build') {
+            sh 'mvn clean package'
+        }
+
+        stage('Test') {
+            sh 'mvn test'
+        }
+
+        stage('Archive') {
+            archiveArtifacts artifacts : 'target/*.jar',
+            fingerprint:true
+        }
+    }
+
+    post {
+        success {
+            echo 'Build et tests réussis'
+        }
+
+        failure {
+            echo 'Le build a échoué'
+        }
+    }
+}
